c5caedcee0cecfdd4157fc85af74b48eb2aef079
phasi <koumiyom4@gmail,com>
2022-02-05 13:04:01 +0900

    made better portfolio

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ const alphabet = [
 ];
 
 const orchestraButton = document.getElementById("orchestra");
-const tennisButton = doccument.getElementById("tennis");
-const soccerButton = doccument.getElementById("soccer");
+const tennisButton = document.getElementById("tennis");
+const soccerButton = document.getElementById("soccer");
 const ansWord = document.getElementById("ans_word");
 const photo = document.getElementById("photo");
 const nameInput = document.getElementById("name-input");
@@ -37,14 +37,18 @@ const checkButton = document.getElementById("check");
 const answerContainer = document.getElementById("answer_container");
 let names = [];
 const r = alphabet.indexOf("r");
+const y = alphabet.indexOf("y");
+const o = alphabet.indexOf("o");
+const d = [r, y, o];
+console.log(r);
 const checkAcuracy = (name) => {
   let final_distance = "";
   let distance_list = [];
   for (let i = 0; i < name.length; i++) {
-    let x = indexOf(name[i]);
+    let x = alphabet.indexOf(name[i]);
 
     if (x != -1) {
-      distance_list.push(Math.abs(x - r));
+      distance_list.push(Math.abs(x - d[i]));
     } else {
       distance_list.push("NaN");
     }
@@ -70,6 +74,7 @@ checkButton.onclick = () => {
     div.textContent = `${names[i].name} || ${contennt}`;
     answerContainer.append(div);
   }
+  nameInput.value = "";
 };
 
 orchestraButton.onclick = () => {
@@ -81,14 +86,14 @@ orchestraButton.onclick = () => {
 
 tennisButton.onclick = () => {
   const div = document.createElement("div");
-  ansWord.textContent = "正解！！";
+  ansWord.textContent = "正解！！\n3回戦が自己ベストなので強くはないよ";
   photo.src = "IMG_0104.jpg";
   ansWord.append(div);
 };
 
 soccerButton.onclick = () => {
   const div = document.createElement("div");
-  ansWord.textContent = "部活じゃないよ";
+  ansWord.textContent = "部活じゃないよ。よくやってたけど、違うよ";
   photo.src =
     "https://with-asset.chouseisan.com/uploads/2018/07/22160230/Tlig59ZXVbQyhT9I1WL7SQkIt3GiQ7L1n8tbpWQC.jpeg";
 };
